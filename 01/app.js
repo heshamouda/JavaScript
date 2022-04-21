a69085fb9c2e1f081170f44835229ffe0a6aae12
heshamouda <hesham.ouda@students.fhnw.ch>
2022-04-21 13:02:43 +0200

    file app.js with some variable

---
+++ 01/app.js
@@ -0,0 +1,12 @@
+var myName = "Hesham Ouda";
+console.log(myName);
+myName = "Alan Smith";
+console.log(myName);
+var message;
+console.log(message);
+message = "New message";
+console.log(message);
+
+//letters, number, _ , $
+var $name = "Some name";
+var firstName;
